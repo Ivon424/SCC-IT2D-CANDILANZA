32e227367b22d6f7eb76939d55c7953fadde217a
Ivon424 <ivon11ki@gmail.com>
2024-09-20 15:05:10 +0800

    Prelim

---
+++ src/ivon/main.java
@@ -0,0 +1,15 @@
+
+package ivon;
+public class main {
+    public static void main(String[] args) {
+        
+        //Account acc=new Account();
+        //acc.Input();
+        
+        //product pp = new product();
+        //pp.manageProduct();
+        
+        //Grade gr= new Grade();
+        //gr.grade();
+    }
+}
